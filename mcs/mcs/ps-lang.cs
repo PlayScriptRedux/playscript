697f420d7266897b2588b532af923901d27e1360
Ben Cooley <bcooley@zynga.com>
2013-04-03 14:15:36 -0700

    Add correct copyright notices (apache) for Zynga contributions.

--- mcs/mcs/ps-lang.cs
+++ mcs/mcs/ps-lang.cs
@@ -1,14 +1,16 @@
+// Copyright 2013 Zynga Inc.
+//	
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// ps-lang.cs: PlayScript language support
-//
-// Author: Ben Cooley (bcooley@zynga.com)
-//
-// Dual licensed under the terms of the MIT X11 or GNU GPL
-//
-// Copyright 2001, 2002 Ximian, Inc (http://www.ximian.com)
-// Copyright 2004-2008 Novell, Inc
-// Copyright 2011 Xamarin, Inc (http://www.xamarin.com)
-//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//		
+//      Unless required by applicable law or agreed to in writing, software
+//      distributed under the License is distributed on an "AS IS" BASIS,
+//      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//      See the License for the specific language governing permissions and
+//      limitations under the License.
 
 using System;
 using System.Collections.Generic;
