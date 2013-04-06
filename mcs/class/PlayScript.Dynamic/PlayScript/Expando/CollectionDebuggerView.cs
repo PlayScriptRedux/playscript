783e4f58df08be7f86bdc8297cc83a025c4c5569
Icer Addis <iaddis@zynga.com>
2013-04-05 22:51:10 -0700

    CollectionDebuggerView was in a nested namespace (mistakenly?)

--- mcs/class/PlayScript.Dynamic/PlayScript/Expando/CollectionDebuggerView.cs
+++ mcs/class/PlayScript.Dynamic/PlayScript/Expando/CollectionDebuggerView.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace PlayScript.Expando
 {
@@ -30,51 +32,45 @@ namespace PlayScript.Expando
 	// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 	//
 	
-	using System;
-	using System.Diagnostics;
-	
-	namespace System.Collections.Generic
+	//
+	// Custom debugger type proxy to display collections as arrays
+	//
+	internal sealed class CollectionDebuggerView<T>
 	{
-		//
-		// Custom debugger type proxy to display collections as arrays
-		//
-		internal sealed class CollectionDebuggerView<T>
+		readonly ICollection<T> c;
+		
+		public CollectionDebuggerView (ICollection<T> col)
 		{
-			readonly ICollection<T> c;
-			
-			public CollectionDebuggerView (ICollection<T> col)
-			{
-				this.c = col;
-			}
-			
-			[DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
-			public T[] Items {
-				get {
-					var o = new T [c.Count];
-					c.CopyTo (o, 0);
-					return o;
-				}
-			}
+			this.c = col;
 		}
 		
-		internal sealed class CollectionDebuggerView<T, U>
-		{
-			readonly ICollection<KeyValuePair<T, U>> c;
-			
-			public CollectionDebuggerView (ICollection<KeyValuePair<T, U>> col)
-			{
-				this.c = col;
+		[DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+		public T[] Items {
+			get {
+				var o = new T [c.Count];
+				c.CopyTo (o, 0);
+				return o;
 			}
-			
-			[DebuggerBrowsable (DebuggerBrowsableState.RootHidden)]
-			public KeyValuePair<T, U>[] Items {
-				get {
-					var o = new KeyValuePair<T, U> [c.Count];
-					c.CopyTo (o, 0);
-					return o;
-				}
-			}
-		}	
+		}
 	}
+	
+	internal sealed class CollectionDebuggerView<T, U>
+	{
+		readonly ICollection<KeyValuePair<T, U>> c;
+		
+		public CollectionDebuggerView (ICollection<KeyValuePair<T, U>> col)
+		{
+			this.c = col;
+		}
+		
+		[DebuggerBrowsable (DebuggerBrowsableState.RootHidden)]
+		public KeyValuePair<T, U>[] Items {
+			get {
+				var o = new KeyValuePair<T, U> [c.Count];
+				c.CopyTo (o, 0);
+				return o;
+			}
+		}
+	}	
 }
 
