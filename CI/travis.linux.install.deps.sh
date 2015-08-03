49c411b8cc27e481b18425005857ea3af96fb2ec
SushiHangover <sushihangover@outlook.com>
2015-08-03 11:24:36 -0700

    Travis enabled OSX testing
    Sure thing- I've just enabled multi-os support for your repository PlayScriptRedux/playscript - please note that it's currently in beta, so please let us know if there is anything you feel can be improved or anything you miss.
    
    Here's what you need to add in your .travis.yml: http://docs.travis-ci.com/user/multi-os/#Setting-.travis.yml
    
    Let us know if there's anything else we can help with.

--- CI/travis.linux.install.deps.sh
+++ CI/travis.linux.install.deps.sh
@@ -5,6 +5,7 @@ if [ -z ${TRAVIS+x} ]
 then
   # Ensure that all required packages are installed.
   sudo apt-get update -qq
-  sudo apt-get install -qq git autoconf libtool automake build-essential mono-devel gettext
+  sudo apt-get install -qq git autoconf libtool automake build-essential mono-devel gettexta
+  sudo apt-get install -qq libtool-bin
 fi
 
