68f4decc3498bac0d692a4ce2d85d469b141e595
SushiHangover <sushihangover@outlook.com>
2015-08-02 23:14:04 -0700

    Travis updates

--- CI/play.regression.sh
+++ CI/play.regression.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ROOT=${PWD}
 ./runtime/mono-wrapper --version
-pushd mcs/playc_tests
+cd mcs/playc_tests
 make clean
 make run-test-local 2>&1 | tee -a ${ROOT}/regressions/play.`git log -n 1 --date=short --pretty=format:"%cd-%h"`.log
 
--- playscript-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Run this to generate all the initial makefiles, make them and install mono to $HOME/playscript-install
-# Ripped off from autogen.sh
-
-echo "Building PlayScript mono framework to $HOME/playscript-install folder"
-./autogen.sh --prefix=$HOME/playscript-install --with-glib=embedded --enable-nls=no --host=x86_64-apple-darwin10
-make
-make install
-
