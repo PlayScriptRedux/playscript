9fea77d35b85030a929716e6173a0e90e9bfb24d
SushiHangover <sushihangover@outlook.com>
2015-08-05 16:34:17 -0700

    configure/make updates

--- autogen.sh
+++ autogen.sh
@@ -162,7 +162,7 @@ conf_flags="--enable-maintainer-mode --enable-compile-warnings" #--enable-iso-c
 
 if test x$NOCONFIGURE = x; then
   echo Running $srcdir/configure $conf_flags "$@" ...
-  $srcdir/configure $conf_flags "$@" \
+  $srcdir/configure --config-cache=CONFIGURE.CACHE $conf_flags "$@" \
   && echo Now type \`make\' to compile $PKG_NAME || exit 1
 else
   echo Skipping configure process.
