69ee1e06b61de9895b20728d0c4f506f2c143809
SushiHangover <sushihangover@outlook.com>
2015-08-06 22:19:00 -0700

    Fix travis, if build success, run mono and play regressions and report

---
+++ CI/travis.regresssion.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+build_exit=0
+play_exit=0
+mono_exit=0
+
+./CI/play.${TRAVIS_OS_NAME}.build.small.i386.sh
+build_exit=$?
+build_exit=0
+if [ $build_exit -eq 0 ]
+then
+  echo "Build passed, running regressions"
+  #./CI/mono.regression.sh
+  mono_exit=$? 
+  ./CI/play.regression.sh
+  play_exit=$?
+else
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  echo "     Mono Build Failed      "
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+fi
+
+if [ $mono_exit -eq 1 ]
+then
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  echo "   Mono Regression Failed   "
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+fi
+
+if [ $play_exit -eq 1 ]
+then
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  echo "   Play Regression Failed   "
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+fi
+
+exit_code=var=$(( build_exit + play_exit + mono_exit  ))
+
+exit $(( exit_code ))
