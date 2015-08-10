4da1b40775b2cba79a99c9e2aa00cc4f754fb5db
SushiHangover <sushihangover@outlook.com>
2015-08-10 15:39:01 -0700

    Report the correct exit code for Travis CI

--- CI/play.regression.sh
+++ CI/play.regression.sh
@@ -3,6 +3,8 @@ ROOT=${PWD}
 ./runtime/mono-wrapper --version
 cd mcs/playc_tests
 make clean
-make run-test-local 2>&1 | tee -a ${ROOT}/regressions/play.`git log -n 1 --date=short --pretty=format:"%cd-%h"`.log
-
-#exit $?
+#make run-test-local 2>&1 | tee -a ${ROOT}/regressions/play.`git log -n 1 --date=short --pretty=format:"%cd-%h"`.log
+make run-test-local
+exit_code=$?
+echo "Exit code :" $exit_code
+exit $exit_code
