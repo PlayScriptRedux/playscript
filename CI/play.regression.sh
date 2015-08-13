20a6e937b3127959344198f7d11d63b1aa13326f
SushiHangover <sushihangover@outlook.com>
2015-08-12 20:52:33 -0700

    Issue #51 - C# (int)(0.7f * 100.0f) == 69 / AS int(0.7 * 100.0) == 70 (Mono #15463)

--- CI/play.regression.sh
+++ CI/play.regression.sh
@@ -3,8 +3,7 @@ ROOT=${PWD}
 ./runtime/mono-wrapper --version
 cd mcs/playc_tests
 make clean
-#make run-test-local 2>&1 | tee -a ${ROOT}/regressions/play.`git log -n 1 --date=short --pretty=format:"%cd-%h"`.log
-make run-test-local
+make check
 exit_code=$?
 echo "Exit code :" $exit_code
 exit $exit_code
